18b545598d8437cf2a885dfdaf375f83bf266499
Wayne Maurer <wayne_maurer@yahoo.com.au>
2015-10-12 22:30:19 +0200

    css loader and extensible webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,27 @@
 var path = require("path");
 var HtmlwebpackPlugin = require("html-webpack-plugin");
 var webpack = require('webpack');
+var merge = require('webpack-merge');
 
+var TARGET = process.env.npm_lifecycle_event;
 var ROOT_PATH = path.resolve(__dirname);
 var APP_PATH = path.resolve(ROOT_PATH, "app");
 var BUILD_PATH = path.resolve(ROOT_PATH, "build");
 
-module.exports = {
+var common = {
 	entry: APP_PATH,
 	output: {
 		path: BUILD_PATH,
 		filename: "bundle.js"
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.css$/,
+				loaders: ['style', 'css'],
+				include: APP_PATH
+			}
+		]
 	},
 	devServer: {
 		historyApiFallback: true,
@@ -19,9 +30,23 @@ module.exports = {
 		progress: true
 	},
 	plugins: [
-		new webpack.HotModuleReplacementPlugin(),
 		new HtmlwebpackPlugin({
 			title: "Kanban app"
 		})
 	]
 };
+
+if(TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devtool: 'eval-source-map',
+    devServer: {
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  });
+}
