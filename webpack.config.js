1f532643f28e0b57c95c7bfc6002f4910d194fc1
Wayne Maurer <wayne_maurer@yahoo.com.au>
2015-10-17 23:07:11 +0200

    build kanban app for prod

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,10 @@ var path = require("path");
 var HtmlwebpackPlugin = require("html-webpack-plugin");
 var webpack = require("webpack");
 var merge = require("webpack-merge");
+var Clean = require("clean-webpack-plugin");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+var pkg = require("./package.json");
 
 var TARGET = process.env.npm_lifecycle_event;
 var ROOT_PATH = path.resolve(__dirname);
@@ -22,21 +26,10 @@ var common = {
 	module: {
 		loaders: [
 			{
-				test: /\.css$/,
-				loaders: ["style", "css"],
-				include: APP_PATH
-			},
-			{
 				test: /\.jsx?$/,
 				loaders: ["babel"],
 				include: APP_PATH
 			}]
-	},
-	devServer: {
-		historyApiFallback: true,
-		hot: true,
-		inline: true,
-		progress: true
 	},
 	plugins: [
 		new HtmlwebpackPlugin({
@@ -56,6 +49,47 @@ if (TARGET === "start" || !TARGET) {
 		},
 		plugins: [
 			new webpack.HotModuleReplacementPlugin()
+		]
+	});
+}
+
+if (TARGET === "build" || TARGET === "stats") {
+	module.exports = merge(common, {
+		entry: {
+			app: APP_PATH,
+			vendor: Object.keys(pkg.dependencies)
+		},
+		output: {
+			path: BUILD_PATH,
+			filename: "[name].[chunkhash].js"
+		},
+		devtool: "source-map",
+		module: {
+			loaders: [
+				{
+					test: /\.css$/,
+		            loader: ExtractTextPlugin.extract('style', 'css'),
+		            include: APP_PATH
+				}
+			]
+		},
+		plugins: [
+			new Clean(['build']),
+	        new ExtractTextPlugin('styles.[chunkhash].css'),
+			new webpack.optimize.CommonsChunkPlugin(
+				'vendor',
+				'[name].[chunkhash].js'
+	        ),
+			new webpack.DefinePlugin({
+				"process.env": {
+					"NODE_ENV": JSON.stringify("production")
+				}
+			}),
+			new webpack.optimize.UglifyJsPlugin({
+				compress: {
+					warnings: false
+				}
+			})
 		]
 	});
 }
